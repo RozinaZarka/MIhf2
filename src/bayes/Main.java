c0ccfbeceb866585b048c0aa701f0b04f9caac7e
RozinaZarka <rozi@zarka.hu>
2018-11-22 15:23:54 +0100

    building vocab try1

--- src/bayes/Main.java
+++ src/bayes/Main.java
@@ -7,7 +7,7 @@ public class Main {
     static int n = 80000;
     static int m = 20000;
     static ArrayList<storedSentence> data = new ArrayList<storedSentence>();
-    static Vocab vocabulary= new Vocab();
+    static ArrayList<VocabElement> vocabulary= new ArrayList<>();
 
     public static void main(String[] args){
         readInput();
@@ -61,26 +61,39 @@ public class Main {
      * @param data
      * @param vocabulary
      */
- public static void makeVocab (ArrayList<storedSentence> data, Vocab vocabulary){
+ public static void makeVocab (ArrayList<storedSentence> data, ArrayList<VocabElement> vocabulary){
      VocabElement temp = new VocabElement();
+
      for(int i = 0;i<n;i++){
+
          for(int j = 0; j < data.get(i).splitted_data.length;j++){
-             System.out.print(data.get(i).splitted_data[j]+' ');
+            // System.out.print(data.get(i).splitted_data[j]+' ');
 
              temp.word = data.get(i).splitted_data[j];
              if (data.get(i).sentiment == 1){
                  temp.positive_sentiment_count += 1;
              } else temp.negative_sentiment_count +=1;
 
+    /** Find out if the word is already in the vocab or not.
+     * If no, set the vocab element to temp.
+     * If yes, set the temp element sentiment counts to the new count, and set the vocab element to the word.
+    */
 
-             if (vocabulary.find(temp.word) == new VocabElement() ){
-
-                 vocabulary.vocabElements.set(i,temp);
+             if (!vocabulary.contains(temp)){
+                 vocabulary.add(temp);
+                 System.out.println("New item in vocab");
+             } else{
+                 int vocabLine = vocabulary.indexOf(temp);
+                 temp.negative_sentiment_count += ( vocabulary.get(i).negative_sentiment_count -1 );
+                 temp.positive_sentiment_count += ( vocabulary.get(i).positive_sentiment_count -1 );
+                 vocabulary.set(vocabLine,temp);
+                 System.out.println("Sentiment count changed for" + temp);
              }
 
 
          }
-         System.out.print('\n');
+         System.out.println("Stored sentence vocabized");
+
      }
 
  }
@@ -89,7 +102,7 @@ public class Main {
      * Evaluates the given data according to the Naive Bayes Algorithm
      * @param vocabulary the vocab
      */
- public static void bayesAlgorithm (Vocab vocabulary){
+ public static void bayesAlgorithm (ArrayList<VocabElement> vocabulary){
 
  }
 }
